951cf1673ff19084e40ef49340a1a4582a95e4aa
sahil <sahil@DESKTOP-N494QAT>
2022-10-20 16:51:46 +0530

    Added Disk Defragment Button was added

--- Form1.vb
+++ Form1.vb
@@ -136,4 +136,8 @@
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
         Process.Start("bin/restart_DWM.bat")
     End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Shell("dfrgui.exe")
+    End Sub
 End Class
