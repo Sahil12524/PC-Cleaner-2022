df127be60e795135aa88b6ffe52147d56b2038eb
Sahil12524 <sahilshailu7977460287@gmail.com>
2022-11-22 23:24:35 +0530

    Added dock to all the buttons on the left panel, increase the size of all button on the left panel, Adjusted the padding of the left panel properly to make those button appear properly.

--- Views/MainPage.Designer.vb
+++ Views/MainPage.Designer.vb
@@ -35,6 +35,7 @@ Partial Class MainPage
         Me.Panel8 = New System.Windows.Forms.Panel()
         Me.Panel1 = New System.Windows.Forms.Panel()
         Me.btnMoreInfo = New System.Windows.Forms.Button()
+        Me.Panel2 = New System.Windows.Forms.Panel()
         Me.Panel7.SuspendLayout()
         Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
@@ -50,89 +51,97 @@ Partial Class MainPage
         Me.Panel7.Controls.Add(Me.btnNetTools)
         Me.Panel7.Controls.Add(Me.btnBasicTools)
         Me.Panel7.Controls.Add(Me.btnHome)
+        Me.Panel7.Controls.Add(Me.Panel2)
         Me.Panel7.Dock = System.Windows.Forms.DockStyle.Left
         Me.Panel7.Location = New System.Drawing.Point(0, 0)
         Me.Panel7.Name = "Panel7"
-        Me.Panel7.Padding = New System.Windows.Forms.Padding(0, 15, 0, 0)
         Me.Panel7.Size = New System.Drawing.Size(189, 641)
         Me.Panel7.TabIndex = 14
         '
         'btnAbout
         '
+        Me.btnAbout.Dock = System.Windows.Forms.DockStyle.Top
         Me.btnAbout.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.btnAbout.Location = New System.Drawing.Point(0, 299)
+        Me.btnAbout.Location = New System.Drawing.Point(0, 345)
         Me.btnAbout.Name = "btnAbout"
-        Me.btnAbout.Size = New System.Drawing.Size(189, 35)
+        Me.btnAbout.Size = New System.Drawing.Size(189, 45)
         Me.btnAbout.TabIndex = 7
         Me.btnAbout.Text = "About"
         Me.btnAbout.UseVisualStyleBackColor = True
         '
         'btnWhatsNew
         '
+        Me.btnWhatsNew.Dock = System.Windows.Forms.DockStyle.Top
         Me.btnWhatsNew.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.btnWhatsNew.Location = New System.Drawing.Point(0, 258)
+        Me.btnWhatsNew.Location = New System.Drawing.Point(0, 300)
         Me.btnWhatsNew.Name = "btnWhatsNew"
-        Me.btnWhatsNew.Size = New System.Drawing.Size(189, 35)
+        Me.btnWhatsNew.Size = New System.Drawing.Size(189, 45)
         Me.btnWhatsNew.TabIndex = 6
         Me.btnWhatsNew.Text = "What's New"
         Me.btnWhatsNew.UseVisualStyleBackColor = True
         '
         'btnOtherOptions
         '
+        Me.btnOtherOptions.Dock = System.Windows.Forms.DockStyle.Top
         Me.btnOtherOptions.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.btnOtherOptions.Location = New System.Drawing.Point(0, 217)
+        Me.btnOtherOptions.Location = New System.Drawing.Point(0, 255)
         Me.btnOtherOptions.Name = "btnOtherOptions"
-        Me.btnOtherOptions.Size = New System.Drawing.Size(189, 35)
+        Me.btnOtherOptions.Size = New System.Drawing.Size(189, 45)
         Me.btnOtherOptions.TabIndex = 5
         Me.btnOtherOptions.Text = "Other Options"
         Me.btnOtherOptions.UseVisualStyleBackColor = True
         '
         'btnGameOptimization
         '
+        Me.btnGameOptimization.Dock = System.Windows.Forms.DockStyle.Top
         Me.btnGameOptimization.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.btnGameOptimization.Location = New System.Drawing.Point(0, 176)
+        Me.btnGameOptimization.Location = New System.Drawing.Point(0, 210)
         Me.btnGameOptimization.Name = "btnGameOptimization"
-        Me.btnGameOptimization.Size = New System.Drawing.Size(189, 35)
+        Me.btnGameOptimization.Size = New System.Drawing.Size(189, 45)
         Me.btnGameOptimization.TabIndex = 4
         Me.btnGameOptimization.Text = "Game Optimization"
         Me.btnGameOptimization.UseVisualStyleBackColor = True
         '
         'btnPowerOptions
         '
+        Me.btnPowerOptions.Dock = System.Windows.Forms.DockStyle.Top
         Me.btnPowerOptions.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.btnPowerOptions.Location = New System.Drawing.Point(0, 135)
+        Me.btnPowerOptions.Location = New System.Drawing.Point(0, 165)
         Me.btnPowerOptions.Name = "btnPowerOptions"
-        Me.btnPowerOptions.Size = New System.Drawing.Size(189, 35)
+        Me.btnPowerOptions.Size = New System.Drawing.Size(189, 45)
         Me.btnPowerOptions.TabIndex = 3
         Me.btnPowerOptions.Text = "Power Options"
         Me.btnPowerOptions.UseVisualStyleBackColor = True
         '
         'btnNetTools
         '
+        Me.btnNetTools.Dock = System.Windows.Forms.DockStyle.Top
         Me.btnNetTools.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.btnNetTools.Location = New System.Drawing.Point(0, 94)
+        Me.btnNetTools.Location = New System.Drawing.Point(0, 120)
         Me.btnNetTools.Name = "btnNetTools"
-        Me.btnNetTools.Size = New System.Drawing.Size(189, 35)
+        Me.btnNetTools.Size = New System.Drawing.Size(189, 45)
         Me.btnNetTools.TabIndex = 2
         Me.btnNetTools.Text = "Net Tools"
         Me.btnNetTools.UseVisualStyleBackColor = True
         '
         'btnBasicTools
         '
+        Me.btnBasicTools.Dock = System.Windows.Forms.DockStyle.Top
         Me.btnBasicTools.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.btnBasicTools.Location = New System.Drawing.Point(0, 53)
+        Me.btnBasicTools.Location = New System.Drawing.Point(0, 75)
         Me.btnBasicTools.Name = "btnBasicTools"
-        Me.btnBasicTools.Size = New System.Drawing.Size(189, 35)
+        Me.btnBasicTools.Size = New System.Drawing.Size(189, 45)
         Me.btnBasicTools.TabIndex = 1
         Me.btnBasicTools.Text = "Basic Tools"
         Me.btnBasicTools.UseVisualStyleBackColor = True
         '
         'btnHome
         '
+        Me.btnHome.Dock = System.Windows.Forms.DockStyle.Top
         Me.btnHome.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.btnHome.Location = New System.Drawing.Point(0, 12)
+        Me.btnHome.Location = New System.Drawing.Point(0, 30)
         Me.btnHome.Name = "btnHome"
-        Me.btnHome.Size = New System.Drawing.Size(189, 35)
+        Me.btnHome.Size = New System.Drawing.Size(189, 45)
         Me.btnHome.TabIndex = 0
         Me.btnHome.Text = "Home"
         Me.btnHome.UseVisualStyleBackColor = True
@@ -164,6 +173,14 @@ Partial Class MainPage
         Me.btnMoreInfo.TabIndex = 0
         Me.btnMoreInfo.Text = ">"
         Me.btnMoreInfo.UseVisualStyleBackColor = True
+        '
+        'Panel2
+        '
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel2.Location = New System.Drawing.Point(0, 0)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(189, 30)
+        Me.Panel2.TabIndex = 8
         '
         'MainPage
         '
@@ -200,4 +217,5 @@ Partial Class MainPage
     Friend WithEvents btnGameOptimization As Button
     Friend WithEvents Panel1 As Panel
     Friend WithEvents btnMoreInfo As Button
+    Friend WithEvents Panel2 As Panel
 End Class
